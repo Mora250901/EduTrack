2dc90aa280840de35de94976ce1888ff64b90eef
Mora250901 <omarmora2509@gmail.com>
2026-04-07 10:36:45 -0500

    Login.js
    Cambio de localhost a la url desplegada

--- frontend-web/src/components/Login.js
+++ frontend-web/src/components/Login.js
@@ -12,7 +12,7 @@ function Login({ onLogin }) {
         setError('');
 
         try {
-            const response = await fetch('http://localhost:3000/api/login', {
+            const response = await fetch('https://edutrack-backend-2ycx.onrender.com/api/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password })
